e5070b4964be2f2346ec6317c265bce9beee22df
Paulhus <reid.paulhus@uleth.ca>
2019-03-24 11:48:24 -0600

    Uwamp

---
+++ UwAmp/www/index.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "dealership";
+
+// Create database
+/*
+$sql = "CREATE DATABASE dealership";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+*/
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE customer(
+    name_first text, 
+    name_last text, 
+    DateOfBirth integer, 
+    Gender integer,
+    job_street integer, 
+    job_province text, 
+    job_city text,
+    job_postal_code text );";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
