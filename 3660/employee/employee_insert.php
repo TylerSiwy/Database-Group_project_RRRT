22f7aff0a8ecea8e6788acd8b5724c2670647154
Wenman <ryan.wenman@uleth.ca>
2019-04-02 16:38:32 -0600

    Removing input of E-ID

--- 3660/employee/employee_insert.php
+++ 3660/employee/employee_insert.php
@@ -8,63 +8,36 @@
 	$dbname = getdbName();
 
 	// Variables for inserting into Customer table
-	$EID = $_POST["eid_input"];
+	//$EID = $_POST["eid_input"];
 	$first = $_POST["first_input"];
     $last = $_POST["last_input"];
 	$phone_number = $_POST["phone_input"];
-        
-        // R CHANGES
+     /////////////////////////   
         $con = new mysqli($servername, $username, $password, $dbname);
         if ($con->connect_error) {
-die("Connection failed: " . $con->connect_error);
-}
-        $temp = "000";
-        $zero_start = 0;
-       $sql= "SELECT eid FROM Employee ORDER BY eid asc";
-           $result = $con->query($sql);
-      //  $num_employees = $result->fetch_assoc();
-        //  echo $num_employees["num_E"]." ";
-       // for($i=0;i<$row; i++){
-          // if ($result->num_rows > 0) 
-	       //   {
-              $row = $result->fetch_assoc();
-               while($row) 
-               {
-                   
-                 //  echo $row["eid"]." ";
-                   $array = explode('-', $row["eid"]);
-               //   echo $array[1]."ECHO SPLIT 1 ";
-                   if($temp == $array[1]){
-                      ++$temp;
-                   }
-                   $row = $result->fetch_assoc();
-               }
-           //}
-        if(strlen($temp)==1){
-            //echo "00".$temp;
-            $EID = "E-00".$temp;
-        }else if(strlen($temp)==2){
-       // echo  $temp."TEMPP";
-        $EID = "E-0".$temp;
+             die("Connection failed: " . $con->connect_error);
         }
+        $temp = "000";
+        $sql= "SELECT eid FROM Employee ORDER BY eid asc";
+        $result = $con->query($sql);
+        $row = $result->fetch_assoc();
+        while($row){
+           $array = explode('-', $row["eid"]);
+           if($temp == $array[1]){
+              ++$temp;
+           }
+           $row = $result->fetch_assoc();
+        }
+        if(strlen($temp)==1){
+            $EID = "E-00".$temp;
+        }
+        else if(strlen($temp)==2){
+            $EID = "E-0".$temp;
+        }
+        else{
+            $EID = "E-".$temp;}
         
-        else{$EID = "E-".$temp;}
-       // $EID = "E-".$temp;
-      //  $EID = "E-".$row;
-//echo $result;
-
-       // $EID = "@";
-     //   $str = $EID;
-       // echo explode('-', $str,1)."<br>";
-       // print_r(explode('-', $str,2));
-     //   $array = explode('-', $str,2);
-     //   $EID = $array[1];
-     //   echo $EID."<br>";
-    //    $EID = ++$EID;
-    //    echo $EID."<br>";
-        // R CHANGES
-        
-        
+     ///////////////////////////////////   
         
 	// Insertion into Employee
 	$con = new mysqli($servername, $username, $password, $dbname);
