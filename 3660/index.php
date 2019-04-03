64f698640ff0677270904982bc639851318fd03f
Paulhus <reid.paulhus@uleth.ca>
2019-04-03 17:09:27 -0600

    Added customer balances button

--- 3660/index.php
+++ 3660/index.php
@@ -39,6 +39,7 @@ include 'database_initial_setup.php';
             <li><a href="customer/Update_Customer.html">Update</a></li>
             <li><a href="customer/Delete_Customer.html">Delete</a></li>
             <li><a href="customer/Delete_Customer.html">Add Employ History</a></li>
+            <li><a href="customer/customer_balances.php">Customer Balances</a></li>
         </ul>
 
         <li><a href="#">Employee</a>
