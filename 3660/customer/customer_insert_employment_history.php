ddb4dc67963589381d6168f5d3870aeb957817a4
Tyler Siwy <tyler.siwy@outlook.com>
2019-04-03 17:32:48 -0600

    fixing late payments

--- 3660/customer/customer_insert_employment_history.php
+++ 3660/customer/customer_insert_employment_history.php
@@ -10,12 +10,6 @@
 	// Variables for inserting into Customer table
 	$first = $_POST["first_input"];
     $last = $_POST["last_input"];
-    $dob = $_POST["dob_input"];
-    $gender = $_POST["gender_input"];
-    $street = (int)$_POST["street_input"];
-    $province = $_POST["province_input"];
-    $city = $_POST["city_input"];
-    $postal_code = $_POST["postal_input"];
 	$phone_number = $_POST["phone_input"];
 	
 	//Variables for inserting into employment history table
@@ -28,25 +22,6 @@
 	$company_province = $_POST["company_province_input"];
 	$company_city = $_POST["company_city_input"];
 	$company_postal_code = $_POST["company_postal_code_input"];
-
-	// Insertion into customer 
-	$con = new mysqli($servername, $username, $password, $dbname);
-
-	if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-	}
-	$sql = "INSERT INTO customer (first_name, last_name, dob, gender, street, province, city, postal_code, phone_number)
-	VALUES (\"$first\", \"$last\", \"$dob\", \"$gender\", \"$street\", \"$province\", \"$city\", \"$postal_code\", \"$phone_number\")";
-
-	// Check if it worked correctly
-	if(mysqli_query($con, $sql)){
-    echo "Records inserted successfully.";
-	} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-	}
-
-	$result = mysqli_query($con, $sql) or trigger_error(mysqli_error($con));	
-	mysqli_close($con);
 
 	//Insertion into employment history
 	$con = new mysqli($servername, $username, $password, $dbname);
